95d34f1ce5f6bd6c3da9e0241c07329a4ca6c603
Fernando Nicolas <ffortizn@gmail.com>
2019-11-23 13:40:49 -0800

    Manual testing for functionality

--- search.js
+++ search.js
@@ -10,17 +10,17 @@ $('form').submit(function(event){
     
     
     var articleInput = $('#searchTerm').val();
-    console.log(articleInput)
+    console.log(articleInput);
     var numOf = $('#numberOfRecords').val();
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+articleInput+"&api-key=XkuwlndL7Pp81qHZ9vEer4a6fLAPiN9J";
+    console.log(queryURL);
     
-    
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-          console.log(response)
-      });
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    //   }).then(function(response) {
+    //       console.log(response);
+    //   });
 
 })
 });
